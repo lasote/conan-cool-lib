b613c0b8603ee57f2a9e597ef31cb9623cba7b81
Luis Martinez de Bartolome <lasote@gmail.com>
2019-02-27 08:49:25 +0100

    zlib 1.2.11 dep

--- conanfile.py
+++ conanfile.py
@@ -14,6 +14,7 @@ class MycoollibConan(ConanFile):
     default_options = "shared=False"
     generators = "cmake"
     exports_sources = "src/*"
+    requires = "zlib/1.2.11@conan/stable"
 
     def build(self):
         cmake = CMake(self)
